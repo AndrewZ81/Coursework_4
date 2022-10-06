449ed9f7e4f93db1d3ca46f58c2ccfbb2d99df3c
Andrew Zaiko <andrewzaiko@gmail.com>
2022-10-06 11:56:24 +0300

    Добавил класс Favorite и обновил остальные классы

--- app/models.py
+++ app/models.py
@@ -4,36 +4,48 @@ from app.create_db import db
 # Создаём класс сущностей "Режиссёр" базы данных
 class Director(db.Model):
     __tablename__ = 'director'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
 
 
 # Создаём класс сущностей "Жанр" базы данных
 class Genre(db.Model):
     __tablename__ = 'genre'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
 
 
 # Создаём класс сущностей "Фильм" базы данных
 class Movie(db.Model):
     __tablename__ = 'movie'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    trailer = db.Column(db.String(255))
-    year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
-    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    trailer = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"), nullable=False)
     genre = db.relationship("Genre")
-    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"), nullable=False)
     director = db.relationship("Director")
+    users = db.relationship("User", secondary="favorite")
 
 
 # Создаём класс сущностей "Пользователь" базы данных
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-    role = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    favorite_genre = db.Column(db.String, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    movies = db.relationship("Movie", secondary="favorite")
+
+
+# Создаём класс сущностей "Избранное" базы данных
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"), primary_key=True)
