73c8a1f66fd394cc9b2832d89c28ce67a703a0cb
Andrew Zaiko <andrewzaiko@gmail.com>
2022-10-07 12:42:23 +0300

    Создал маршрут регистрации пользователя

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,36 +1,40 @@
 import json
+import sqlite3
 
 from flask import request, jsonify
 from flask_restx import Resource, Namespace
 
 from app.create_db import db
 from app.models import User
-from app.functions import compare_passwords, generate_tokens, regenerate_tokens, check_token
+from app.functions import compare_passwords, generate_tokens, regenerate_tokens, check_token, get_hash
 
 auth_ns = Namespace("auth")  # Создаём пространство имён аутентификации
 
 
-@auth_ns.route("/")  # Создаём маршрут аутентификации
+@auth_ns.route("/register")  # Создаём маршрут регистрации пользователя
 class AuthView(Resource):
     def post(self):
         post_data = request.json
-        user_name = post_data.get("username")
+        email = post_data.get("email")
         user_password = post_data.get("password")
-        if not user_name:
-            return "Пустое имя пользователя", 404
+        user = User(**post_data)
+        if not email:
+            return "Пустой адрес электронной почты", 404
         elif not user_password:
             return "Пустой пароль", 404
-        users = db.session.query(User).filter(User.username == user_name).all()
-        if not users:
-            return "Неверное имя пользователя", 404
+        user.password = get_hash(post_data.get("password"))
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except sqlite3.OperationalError:
+            db.session.rollback()
+            return "Не удалось добавить пользователя", 404
         else:
-            for i in users:
-                if compare_passwords(i.password, user_password):
-                    post_data["role"] = i.role
-                    response = jsonify(generate_tokens(post_data))
-                    response.status_code = 201
-                    return response
-        return "Неверный пароль", 404
+            response = jsonify(post_data)
+            response.headers['location'] = f'/auth/register/{user.id}'
+            response.status_code = 201
+            return response
+
 
     def put(self):
         put_data = request.json
