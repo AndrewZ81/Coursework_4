5f5b9781bb2cf3e7979f52a8194afa64ddc7ddd6
Andrew Zaiko <andrewzaiko@gmail.com>
2022-10-08 20:51:06 +0300

    Добавил метод DELETE

--- app/views/favorites.py
+++ app/views/favorites.py
@@ -29,12 +29,41 @@ class FavoritesView(Resource):
                     try:
                         engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
                         conn = engine.connect()
-                        ins = favorite.insert().values(user_id = user.id, movie_id = movie_id)
+                        ins = favorite.insert().values(user_id=user.id, movie_id=movie_id)
                         conn.execute(ins)
                     except Exception:
                         return "Не удалось добавить в избранное", 404
                     else:
-                         return "Добавлено в избранное", 200
+                        return "Добавлено в избранное", 200
+            return "Фильма с таким ID не существует", 404
+        else:
+            return "Такого пользователя не существует", 404
+
+    @auth_required
+    def delete(self, movie_id):
+        """
+        Удаляет фильм из избранного
+        :return: Удаляет запись из таблицы favorites
+        """
+
+        # Получаем пользователя из БД по e-mail из токена
+        user = db.session.query(User).filter(User.email == get_user()).first()
+
+        if user:
+            movies = db.session.query(Movie).all()
+            for i in movies:  # Сверяем ID всех фильмов в БД с переданным ID
+                if i.id == movie_id:
+                    try:
+                        engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+                        conn = engine.connect()
+                        delete_favorite = favorite.delete(). \
+                            where(favorite.c.user_id == user.id). \
+                            where(favorite.c.movie_id == movie_id)
+                        conn.execute(delete_favorite)
+                    except Exception:
+                        return "Не удалось удалить из избранного", 404
+                    else:
+                        return "Удалено из избранного", 404
             return "Фильма с таким ID не существует", 404
         else:
             return "Такого пользователя не существует", 404
